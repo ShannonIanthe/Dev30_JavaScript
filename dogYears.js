02cbf0c1ee41798584316546ba39796fa02360f8
Shannon Mettry <shannonmettry12@gmail.com>
2022-04-29 21:35:17 +0200

    adding project 2: Dog Years

---
+++ dogYears.js
@@ -0,0 +1,25 @@
+
+//PROJECT: Dog Years
+
+//assigning the variable myAge the value of my age
+let myAge = 31;
+
+//assigning the variable early years the value of 2
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//laterYears is equal to the myAge variable minus two
+let laterYears = myAge - 2;
+
+//to obtain dog years I add the two variables together
+myAgeInDogYears = earlyYears + laterYears;
+
+//creating the myName variable and assigning it 'Shannon'
+let myName = 'Shannon';
+
+//using the string method to lowercase all the letters in my name
+myName.toLowerCase();
+
+//Using a string interpolation to display the values of the variable in a sentence
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
--- main.js
+++ main.js
@@ -18,3 +18,32 @@ const newton = Math.floor(celsius * (33/100));
 
 console.log(`The tempature is ${newton} degrees Newton.`)
 
+
+//
+//
+//
+
+//assigning the variable myAge the value of my age
+let myAge = 31;
+
+//assigning the variable early years the value of 2
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//laterYears is equal to the myAge variable minus two
+let laterYears = myAge - 2;
+
+//to obtain dog years I add the two variables together
+myAgeInDogYears = earlyYears + laterYears;
+
+//creating the myName variable and assigning it 'Shannon'
+let myName = 'Shannon';
+
+//using the string method to lowercase all the letters in my name
+myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
