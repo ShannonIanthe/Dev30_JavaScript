01f4c1e5ad096b293399c85739714bb34820f147
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-03 20:34:29 +0200

    military project

---
+++ militaryGreeting.js
@@ -0,0 +1,11 @@
+const name ='';
+const rank = '';
+const age = Math.floor(Math.random() * 18);
+
+if (age < 18) {
+    console.log('You are too young for the military!');
+}
+
+if (name === 'Rugen' && rank === 'Count') {
+    console.log('My name is Inigo Montoya, you killed my father, prepare to die.')
+}
