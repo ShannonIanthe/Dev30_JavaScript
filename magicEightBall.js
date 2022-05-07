9ad3fbb2b65948fd77732c7914f93eeca2a4c618
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-07 15:37:03 +0200

    Finished race day project

--- magicEightBall.js
+++ magicEightBall.js
@@ -1,37 +1,36 @@
-const userName = 'Shannon';
+const userName = "Shannon";
 
 //if userName is not empty say hello + userName
-if (userName !== ' ') {
-    console.log(`Hello, ${userName}!`);
+if (userName !== " ") {
+  console.log(`Hello, ${userName}!`);
 } else {
-    console.log('Hello!')
+  console.log("Hello!");
 }
 
-const userQuestion = 'Am I amazing?';
-console.log(`${userName} wants to know ${userQuestion}`);
+const userQuestion = "Am I amazing?";
+console.log(`${userName} asked, ${userQuestion}`);
 
 const randomNumber = Math.floor(Math.random() * 8);
-const eightBall = '';
+const eightBall = "";
 
 if (randomNumber === 1) {
-    console.log('It is certain');
+  console.log("It is certain");
 } else if (randomNumber === 2) {
-    console.log('It is decidedly so');
+  console.log("It is decidedly so");
 } else if (randomNumber === 3) {
-    console.log('Reply hazy try again');
+  console.log("Reply hazy try again");
 } else if (randomNumber === 4) {
-    console.log('Cannot predict now');
-}else if (randomNumber === 5) {
-    console.log('Do not count on it');
+  console.log("Cannot predict now");
+} else if (randomNumber === 5) {
+  console.log("Do not count on it");
 } else if (randomNumber === 6) {
-    console.log('My sources say no');
+  console.log("My sources say no");
 } else if (randomNumber === 7) {
-    console.log('Outlook not so good');
+  console.log("Outlook not so good");
 } else {
-    //if the random number is 0
-    console.log('Signs point to yes')
+  //if the random number is 0
+  console.log("Signs point to yes");
 }
 
-//if the random number is 0
-
 console.log(eightBall);
+
--- raceDay.js
+++ raceDay.js
@@ -1,16 +1,19 @@
-let raceNumber = Math.floor(Math.random() *1000);
+let raceNumber = Math.floor(Math.random() * 1000);
 let registrationEarly = true;
-let age = 34;
+let age = 28;
 
 if (age > 18 && registrationEarly === true) {
     console.log(raceNumber + 1000);
 }
 
 if (age > 18 && registrationEarly === true) {
-    console.log(`You will race at 9:30am ${raceNumber}`)
-} else if ( age > 18 && registrationEarly !== early) {
-    console.log(`You will race at 11:00am. ${raceNumber}`)
+    console.log(`Early adults run at 9:30am. ${raceNumber}`)
+} else if ( age > 18 && registrationEarly !== true) {
+    console.log(`Late adults run at 11:00am. ${raceNumber}`)
 } else if (age < 18) {
-    console.log(`You will run at 12:30pm. ${raceNumber}`)
+    console.log(`Youth registrants run at 12:30 pm (regardless of registration). ${raceNumber}`)
+} else {
+    console.log(`All others will race at 12.30 pm ${raceNumber}`);
 }
+
 
