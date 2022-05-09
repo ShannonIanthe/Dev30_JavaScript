6676a0d5e6834581eea92cbbfb1b0f91d087e3fb
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-09 14:10:52 +0200

    fixed rock paper scissors project

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,32 +1,59 @@
-var getUserChoice = function(userInput) {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' | userInput === 'paper' | userInput === 'scissors') {
-        return userInput
-    } else {
-        console.log('Not an acceptable answer, try again.');
-    }
-}
+var getUserChoice = function (userInput) {
+  userInput = userInput.toLowerCase();
 
-var determineWinner = function(userChoice, computerChoice) {
-    if ( userChoice === computerChoice) {
-        return 'Game was a tie.'
-    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
-        return 'Computer Won!'
-    } else if (userChoice === 'scissors' && computerChoice === 'rock') {
-        return 'Computer Won'
-    } else if (userChoice === 'rock' && computerChoice === 'paper') {
-        return 'Computer won'
-    } else {
-        return 'You won!'
-    }
-}
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "bomb"
+  ) {
+      return userInput;
+  } else {
+    console.log('Not an acceptable answer. You must enter "rock", "paper", or "scissors".');
+  }
+};
 
-var playGame = function() {
-    userChoice === getUserChoice('rock');
-    console.log(userChoice);
-    computerChoice === getComputerChoice('scissors')
-    console.log(computerChoice);
-    console.log(determineWinner(userChoice, computerChoice));
-}
+var getComputerChoice = function () {
+  var randomNumber = Math.floor(Math.random() * 3);
 
-playGame();
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  }
+};
+
+var determineWinner = function (userChoice, computerChoice) {
+  if (userChoice === "bomb") {
+    return "The user won!";
+  } 
+  if (userChoice === computerChoice) {
+    return "Game was a tie.";
+  } else if (userChoice === "paper" && computerChoice === "scissors") {
+    return "Computer Won!";
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    return "Computer Won!";
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    return "Computer won!";
+  } else {
+    return "You won!";
+  }
+};
+
+var playGame = function (userInput) {
+  var userChoice = getUserChoice('rock');
+  var computerChoice = getComputerChoice();
+
+  console.log("User Choice", userChoice);
+  console.log("Computer Choice", computerChoice);
+
+  var winnerText = determineWinner(userChoice, computerChoice);
+
+  console.log(winnerText);
+};
+
+var userInput = prompt();
+
+playGame(userInput);
