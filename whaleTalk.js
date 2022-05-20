cb591a3471b6165f53ded8fa29faa84f7391ce84
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-20 15:11:54 +0200

    for loop project

---
+++ whaleTalk.js
@@ -0,0 +1,26 @@
+var input ='I like to eat fish';
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    const input2 = input[i];
+
+    if (input2 === 'e') {
+        resultArray.push(input[i])
+    }
+
+   for (let j = 0; j < vowels.length; j++) {
+       const vowels2 = vowels[j];
+
+       if (input2 === vowels2) {
+           resultArray.push(input2)
+       }
+       if (input2 === 'u') {
+        resultArray.push(input[i])
+    }
+       
+   }
+}
+
+var resultString = resultArray.join(' ').toUpperCase();
+console.log(resultString);
