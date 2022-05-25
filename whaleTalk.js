3c42f1079bf6547fe4a36aa563b98005d387c7fe
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-25 12:41:23 +0200

    refactoring code on line 8

--- whaleTalk.js
+++ whaleTalk.js
@@ -1,15 +1,12 @@
-var input ='turpentine and turtles';
+var input ='I love to code';
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
     const input2 = input[i];
 
-    if (input2 === 'e') {
-        resultArray.push(input[i])
-    }
-    if (input2 === 'u') {
-        resultArray.push(input[i])
+    if (input2 === 'e' || input2 === 'u') {
+        resultArray.push(input2)
     }
 
    for (let j = 0; j < vowels.length; j++) {
@@ -18,8 +15,6 @@ for (let i = 0; i < input.length; i++) {
        if (input2 === vowels2) {
            resultArray.push(input2)
        }
-
-       
    }
 }
 
