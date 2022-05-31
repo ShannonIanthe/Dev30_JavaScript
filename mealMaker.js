bd9b44a1d117af73b93d48228b29891dc5393406
Shannon Mettry <shannonmettry12@gmail.com>
2022-05-31 16:00:26 +0200

    working on mealMaker

---
+++ mealMaker.js
@@ -0,0 +1,32 @@
+//Step 1
+let menu = {};
+
+//Step 2: add courses property to menu, set it to an empty object
+menu.courses = {};
+
+//Step 3: Create three properties inside the courses object
+menu.courses.appetizers = [];
+menu.courses.main = [];
+menu.courses.desserts = [];
+
+//Step 4 & 5: add method in menu object
+menu.addDishToCourse = function(courseName, dishName, dishPrice) {
+    const dish = {
+        name: [],
+        price: []
+    };
+    this.courses[courseName].push(dish);
+};
+
+//Step 6: Create a method inside the menu object
+menu.getRandomDishFromCourse = function(courseName) {
+//Step 7:
+const dishes = this.courses[courseName]
+const randomDish = floor(Math.random * dishes.length);
+return dishes[randomDish];
+}
+
+
+//Step 8
+
+console.log(dishes);
